fcc7a2de1e30b104262d73cfe81e49bddbb16b0c
nakul sharma <nakul777@gmail.com>
2024-12-26 05:40:47 +0000

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
-s
+import dagshub
+dagshub.init(repo_owner='nakul777', repo_name='AWSIntegration', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
